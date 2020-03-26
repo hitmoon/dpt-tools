d8aa05369145bd76120d41ff2af8bc0497f6a57e
xiaoqiang zhao <zxq_yx_007@163.com>
2020-03-26 12:25:24 +0800

    update cp1.sh with my personal device keys

--- cp1.sh
+++ cp1.sh
@@ -8,4 +8,4 @@ fi
 
 addr=$1
 
-python3 dpt-tools.py -id /home/hitmoon/deviceid.dat -k /home/hitmoon/privatekey.dat -ip "$addr"
+python3 dpt-tools.py -id ./my_cp1_keys/deviceid.dat -k ./my_cp1_keys/privatekey.dat -ip "$addr"
